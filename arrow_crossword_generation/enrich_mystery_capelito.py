2b03b7891cc38e1a05eb08364629e9c60ca9b8d2
Tom Dupisre <tom.dupisre@gmail.com>
2024-07-22 19:40:04 +0200

    ok

--- arrow_crossword_generation/enrich_mystery_capelito.py
+++ arrow_crossword_generation/enrich_mystery_capelito.py
@@ -4,6 +4,7 @@ from collections import namedtuple
 from random import shuffle
 
 import pandas as pd
+from loguru import logger
 
 from arrow_crossword_generation.create_dictionary import clean_dictionary
 from arrow_crossword_generation.utilities.constants import (
@@ -17,10 +18,7 @@ from arrow_crossword_generation.utilities.generation_utilities import (
 from arrow_crossword_generation.utilities.init_utilities import (
     get_validated_custom_words,
 )
-from shared_utilities.capelito.utilities import (
-    save_capelitos_to_json,
-    init_capelitos,
-)
+from shared_utilities.arrow_crossword.arrow_crossword import ArrowCrossword
 
 MysteryLetter = namedtuple(
     "MysteryLetter",
@@ -63,39 +61,29 @@ def sort_dict_by_unused_words(magazine_letters, word_letters):
         value.sort(key=lambda x: (x.i in i_already_used) or (x.j in j_already_used), reverse=True)
     return magazine_letters
 
-def get_mystery_capelito(df_game_state, custom_possible_words) -> dict:
+def enrich_mystery_capelito_2(arrow_crossword: ArrowCrossword, custom_possible_words: list[str]) -> ArrowCrossword:
     letters = []
-    for i in range(len(df_game_state)):
-        for j in range(len(df_game_state[i])):
-            if not df_game_state[i][j].isnumeric():
-                letters.append(MysteryLetter(letter=df_game_state[i][j], i=i, j=j))
+    for i in range(len(arrow_crossword.game_state)):
+        for j in range(len(arrow_crossword.game_state[i])):
+            if not arrow_crossword.game_state[i][j].isnumeric():
+                letters.append(MysteryLetter(letter=arrow_crossword.game_state[i][j], i=i, j=j))
 
     shuffle(custom_possible_words)
 
     word_letters = pick_a_custom_word_from_state(custom_possible_words, letters)
     word = "".join([l.letter for l in word_letters])
-    custom_word_to_export = {
+    arrow_crossword.mystery_capelito = {
         "word": word,
         "word_letters": word_letters,
         "definition": word,
     }
 
-    return custom_word_to_export
+    return arrow_crossword
 
 
-def enrich_mystery_capelito(capelito_filename: str):
-    f = open(f"data/capelitos/{capelito_filename}.json")
-    filled_map_json = json.load(f)
-    capelitos = init_capelitos(filled_map_json)
-    df_map = pd.read_csv(
-        f"resources/maps/{filled_map_json["map_file"]}.csv", header=None, sep=","
-    )
-    df_game_state = df_map.values.tolist()
-    for capelito in capelitos:
-        df_game_state = set_letters(capelito, df_game_state)
-
+def enrich_mystery_capelito(arrow_crossword: ArrowCrossword) -> ArrowCrossword:
+    logger.info('Enrichment of Mystery Capelito is starting...')
     validated_custom_words = get_validated_custom_words()
-
     df_words_file = pd.read_csv(
         f"{DICTIONARY_TO_PATH[DICTIONARY.CUSTOM_DICTIONARY]}/{DICTIONARY.CUSTOM_DICTIONARY}.csv",
         dtype=object,
@@ -104,15 +92,10 @@ def enrich_mystery_capelito(capelito_filename: str):
     custom_possible_words = df_words_file["word"].to_list()
 
     custom_possible_words = clean_custom_possibles_words(
-        custom_possible_words, capelitos, validated_custom_words
+        custom_possible_words, arrow_crossword.capelitos, validated_custom_words
     )
 
-    custom_word_to_export = get_mystery_capelito(df_game_state, custom_possible_words)
+    arrow_crossword = enrich_mystery_capelito_2(arrow_crossword, custom_possible_words)
+    logger.info(f'Found word {arrow_crossword.mystery_capelito["word"]} for mystery capelito')
 
-    save_capelitos_to_json(
-        capelitos=capelitos,
-        score=filled_map_json["score"],
-        map_file=filled_map_json["map_file"],
-        capelito_file=capelito_filename,
-        mystery_capelito=custom_word_to_export,
-    )
+    return arrow_crossword
--- arrow_crossword_generation/utilities/generation_utilities.py
+++ arrow_crossword_generation/utilities/generation_utilities.py
@@ -4,7 +4,7 @@ from random import shuffle
 from loguru import logger
 
 from arrow_crossword_generation.utilities.constants import (
-    NB_CUSTOM_capelitoS_MIN,
+    NB_CUSTOM_CAPELITOS_MIN,
     DICTIONARY,
 )
 from shared_utilities.arrowed_place_holder.arrowed_place_holder import (
@@ -17,7 +17,7 @@ def shuffle_capelitos(capelitos: list[Capelito]) -> list[Capelito]:
     # We only keep min 3 length words
     custom_capelitos = [d for d in capelitos if len(d.word) > 2]
     shuffle(custom_capelitos)
-    custom_capelitos = custom_capelitos[:NB_CUSTOM_capelitoS_MIN]
+    custom_capelitos = custom_capelitos[:NB_CUSTOM_CAPELITOS_MIN]
 
     capelitos = [d for d in capelitos if d not in custom_capelitos]
     capelitos = sorted(capelitos, key=lambda x: len(x.word), reverse=True)
@@ -64,7 +64,7 @@ def update_possible_values(
     every_capelito_has_solution = True
     for capelito in capelitos:
         cursor += 1
-        should_be_custom = (cursor <= NB_CUSTOM_capelitoS_MIN)
+        should_be_custom = (cursor <= NB_CUSTOM_CAPELITOS_MIN)
         if capelito.word in all_dictionaries[DICTIONARY.FORBIDDEN_DICTIONARY][len(capelito.word)] and not should_be_custom:
             every_capelito_has_solution = False
             break
